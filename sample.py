6c74b911e4007f92606d2606a780a2369c1cf64c
yanaapatelbscibm2025-design <yanaapatel_bscibm2025@iar.ac.in>
2026-01-23 10:35:06 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,12 @@
 a = 10
 b = 20
-c =40
-if(a>b and a>c):
+c = 40
+d = 55
+if(a>b and a>c and a>d):
   print("a is greater")
-elif(b>c and b>c):
+elif(b>c and b>c and b>d):
   print("b is greater")
+elif(c>d and c>a and c>b):
+  print("c is greater")
 else:
-  ptint("c is greater")
+  ptint("d is greater")
